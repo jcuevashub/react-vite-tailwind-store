c3371f9ed743cb5616ee767222517722b885491c
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-07-06 10:33:01 -0400

    Componente Navbar

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -5,6 +5,7 @@ import MyOrder from "../MyOrder"
 import MyOrders from "../MyOrders"
 import NotFound from "../NotFound"
 import SignIn from "../SignIn"
+import Navbar from '../../Componets/Navbar'
 
 
 const AppRoutes = () => {
@@ -24,6 +25,7 @@ function App() {
     return (
         <BrowserRouter>
             <AppRoutes />
+            <Navbar />
         </BrowserRouter>
     )
 }
