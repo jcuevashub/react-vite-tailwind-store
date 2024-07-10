fa6fd104274a7653339677cefe4ed3fffb443270
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-07-09 20:12:51 -0400

    Consumiendo la FakeStore API para pintar cards

--- src/Componets/Card/index.jsx
+++ src/Componets/Card/index.jsx
@@ -1,14 +1,14 @@
-const Card = () => {
+const Card = ({price, title, images, category: {name}}) => {
     return (
         <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
             <figure className="relative mb-2 w-full h-4/5">
-                <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">Electronics</span>
-                <img className="w-full h-full object-cover rounded-lg" src="https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Coco" />
+                <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{name}</span>
+                <img className="w-full h-full object-cover rounded-lg" src={images[0]} alt={title} />
                 <button className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1">+</button>
             </figure>
             <p className="flex justify-between">
-                <span className="text-sm font-light">Headphones</span>
-                <span className="text-lg font-medium">$300</span>
+                <span className="text-sm font-light">{title}</span>
+                <span className="text-lg font-medium">{`$${price}`}</span>
             </p>
         </div>
     )
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from "react"
 import Card from "../../Componets/Card"
+import { apiUrl } from "../../api";
 
 function Home() {
-    return (
-      <div>
-        <Card />
-        Hola mundo
-      </div>
-    )
-  }
-  
-  export default Home
-  
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    const fetData = async () => {
+      try {
+        const response = await fetch(`${apiUrl}/products`)
+        const data = await response.json()
+        setItems(data)
+      } catch (error) {
+        console.error(`Oh no, ocurrió un error: ${error}`)
+      }
+    }
+    fetData()
+  }, [])
+
+  return (
+    <div>
+      <section className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {
+          items?.map(item => (
+            <Card key={item.id} {...item} />
+          ))
+        }
+      </section>
+
+    </div>
+  )
+}
+
+export default Home
