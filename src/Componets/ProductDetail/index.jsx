22da05b535dd5177c54d60c7587beb15bbcdae24
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-07-10 20:34:35 -0400

    Maquetando el ProductDetail

--- src/Componets/ProductDetail/index.jsx
+++ src/Componets/ProductDetail/index.jsx
@@ -1,9 +1,14 @@
+import { useContext } from 'react';
+import { ShoppingCartContext } from '../../Context'
+
 const ProductDetail = () => {
+    const context = useContext(ShoppingCartContext)
+
     return (
-        <aside className="flex flex-col fixed right-0 border bg-white border-black rounded-lg w-[360px] h-[calc(100vh-80px)]">
+        <aside className={`${context.isProductDetailOpen ? 'flex' : 'hidden'}  flex-col fixed right-0 border bg-white border-black rounded-lg w-[360px] h-[calc(100vh-80px)]`}>
             <div className="flex justify-between items-center p-6">
                 <h2 className="font-medium text-xl">Detail</h2>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <svg  onClick={() => context.toggleProductDetail()} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
                 </svg>
             </div>
