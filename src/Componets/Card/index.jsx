5f9e8fe5deeeb29773e4b4dea463c4f327c08f52
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-07-10 20:16:29 -0400

    Reto: heroicons con TailwindCSS

--- src/Componets/Card/index.jsx
+++ src/Componets/Card/index.jsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import { ShoppingCartContext } from '../../Context'
 
-const Card = ({price, title, images, category: {name}}) => {
+const Card = ({ price, title, images, category: { name } }) => {
     const context = useContext(ShoppingCartContext)
 
     return (
@@ -9,10 +9,11 @@ const Card = ({price, title, images, category: {name}}) => {
             <figure className="relative mb-2 w-full h-4/5">
                 <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{name}</span>
                 <img className="w-full h-full object-cover rounded-lg" src={images[0]} alt={title} />
-                <button 
-                    className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1"
-                    onClick={() => context.setCount(context.count + 1)}
-                    >+</button>
+                <button className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1" onClick={() => context.setCount(context.count + 1)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    </svg>
+                </button>
             </figure>
             <p className="flex justify-between">
                 <span className="text-sm font-light">{title}</span>
--- src/Componets/ProductDetail/index.jsx
+++ src/Componets/ProductDetail/index.jsx
@@ -1,6 +1,13 @@
 const ProductDetail = () => {
     return (
-        <aside className="flex flex-col fixed right-0 border bg-white border-black rounded-lg w-[360px] h-[calc(100vh-80px)]"></aside>
+        <aside className="flex flex-col fixed right-0 border bg-white border-black rounded-lg w-[360px] h-[calc(100vh-80px)]">
+            <div className="flex justify-between items-center p-6">
+                <h2 className="font-medium text-xl">Detail</h2>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                </svg>
+            </div>
+        </aside>
     )
 }
 
