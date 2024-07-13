3978105bbfe358a359db68ed757678e0381fa915
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-07-13 11:19:23 -0400

    Componente OrderCard

--- src/Componets/CheckoutSideMenu/index.jsx
+++ src/Componets/CheckoutSideMenu/index.jsx
@@ -2,20 +2,33 @@ import { useContext } from 'react';
 import { ShoppingCartContext } from '../../Context'
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import './styles.css'
+import OrderCard from '../OrderCard';
 
 const CheckoutSideMenu = () => {
     const context = useContext(ShoppingCartContext)
-
+    console.log( context.cartProducts)
     return (
-        <aside className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'}  flex-col fixed right-0 border bg-white border-black rounded-lg w-[360px] h-[calc(100vh-80px)]`}>
+        <aside className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'}flex-col fixed right-0 border bg-white border-black rounded-lg w-[360px] h-[calc(100vh-80px)]`}>
             <div className="flex justify-between items-center p-6">
                 <h2 className="font-medium text-xl">My Order</h2>
                 <div>
-                    <XMarkIcon 
+                    <XMarkIcon
                         className='h-6 w-6 text-black cursor-pointer'
                         onClick={() => context.closeCheckoutSideMenu()}
                     />
                 </div>
+            </div>
+            <div className='px-6'>
+            {
+                context.cartProducts.map(product => (
+                    <OrderCard
+                        key={product.id}
+                        title={product.title}
+                        imageUrl={product.images}
+                        price={product.price}
+                    />
+                ))
+            }
             </div>
         </aside>
     )
