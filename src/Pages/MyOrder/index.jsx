5670fd1cc2d99b1172b8163058db81923f3ae73e
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-07-14 15:24:53 -0400

    Página de MyOrder: órden individual

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -6,7 +6,10 @@ import { ChevronLeftIcon } from '@heroicons/react/24/solid';
 
 function MyOrder() {
     const context = useContext(ShoppingCartContext)
-
+    const currentPath = window.location.pathname
+    let index = currentPath.substring(currentPath.lastIndexOf('/') + 1)
+    if (index === 'last') index = context.order?.length - 1
+    
     return (
         <>
             <div className="flex items-center justify-center w-80 mb-6">
@@ -17,7 +20,7 @@ function MyOrder() {
             </div>
             <div className='flex flex-col w-80'>
                 {
-                    context.order?.slice(-1)[0].products.map(product => (
+                    context.order?.[index]?.products.map(product => (
                         <OrderCard
                             key={product.id}
                             id={product.id}
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,26 +1,25 @@
 import { useContext } from "react"
 import { ShoppingCartContext } from "../../Context"
-import OrderCard from "../../Componets/OrderCard"
 import { Link } from "react-router-dom"
-import { ChevronLeftIcon } from "@heroicons/react/24/solid"
+import OrdersCard from "../../Componets/OrdersCard"
 
 function MyOrders() {
   const context = useContext(ShoppingCartContext)
 
   return (
     <>
-      <div className="flex items-center justify-center w-80 ">
+      <div className="flex items-center justify-center w-80">
         <h1>My Orders</h1>
       </div>
       {
-        context.order.map((order, index) => {
-          <Link to={`/my-orders/${order.id}`}>
-            <OrderCard
+        context.order.map((order, index) => (
+          <Link to={`/my-orders/${index}`}>
+            <OrdersCard
               totalPrice={order.totalPrice}
               totalProducts={order.totalProducts}
             />
           </Link>
-        })
+        ))
       }
     </>
   )
