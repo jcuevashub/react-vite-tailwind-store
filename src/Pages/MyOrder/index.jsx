0e39eb39270fd551ef641c7ce15732614d0411c7
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-07-14 12:14:42 -0400

    Página de MyOrders: lista de órdenes

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,13 +1,20 @@
 import { useContext } from 'react';
 import { ShoppingCartContext } from '../../Context'
 import OrderCard from '../../Componets/OrderCard';
+import { Link } from 'react-router-dom';
+import { ChevronLeftIcon } from '@heroicons/react/24/solid';
 
 function MyOrder() {
     const context = useContext(ShoppingCartContext)
 
     return (
-        <div>
-            MyOrder
+        <>
+            <div className="flex items-center justify-center w-80 mb-6">
+                <Link to='/my-orders'>
+                    <ChevronLeftIcon className="h-6 w-6 text-black cursor-pointer mr-5" />
+                </Link>
+                <h1>My Order</h1>
+            </div>
             <div className='flex flex-col w-80'>
                 {
                     context.order?.slice(-1)[0].products.map(product => (
@@ -21,7 +28,7 @@ function MyOrder() {
                     ))
                 }
             </div>
-        </div>
+        </>
     )
 }
 
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,10 +1,29 @@
+import { useContext } from "react"
+import { ShoppingCartContext } from "../../Context"
+import OrderCard from "../../Componets/OrderCard"
+import { Link } from "react-router-dom"
+import { ChevronLeftIcon } from "@heroicons/react/24/solid"
+
 function MyOrders() {
-    return (
-      <div className='bg-red-200'>
-       MyOrders
+  const context = useContext(ShoppingCartContext)
+
+  return (
+    <>
+      <div className="flex items-center justify-center w-80 ">
+        <h1>My Orders</h1>
       </div>
-    )
-  }
-  
-  export default MyOrders
-  
+      {
+        context.order.map((order, index) => {
+          <Link to={`/my-orders/${order.id}`}>
+            <OrderCard
+              totalPrice={order.totalPrice}
+              totalProducts={order.totalProducts}
+            />
+          </Link>
+        })
+      }
+    </>
+  )
+}
+
+export default MyOrders
