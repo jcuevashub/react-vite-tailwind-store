bdc3f912ae3c38599225ac86fc752849dd3852ec
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-07-14 10:36:23 -0400

    Flujo para crear una nueva orden

--- src/Componets/CheckoutSideMenu/index.jsx
+++ src/Componets/CheckoutSideMenu/index.jsx
@@ -13,6 +13,19 @@ const CheckoutSideMenu = () => {
         context.setCartProducts(filteredProduct)
     }
 
+    const handleCheckout = () => {
+        const orderToAdd = {
+            date: '01.02.23',
+            products: context.cartProducts,
+            totalProducts: context.cartProducts.length,
+            totalPrice: totalPrice(context.cartProducts)
+        }
+
+        context.setOrder([...context.order, orderToAdd])
+        context.setCount(0)
+        context.setCartProducts([])
+    }
+
     return (
         <aside className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} z-10 top-98 flex-col fixed right-0 border bg-white border-black rounded-lg w-[360px] h-[calc(100vh-80px)]`}>
             <div className="flex justify-between items-center p-6">
@@ -24,25 +37,26 @@ const CheckoutSideMenu = () => {
                     />
                 </div>
             </div>
-            <div className='px-6 overflow-y-scroll'>
-            {
-                context.cartProducts.map(product => (
-                    <OrderCard
-                        key={product.id}
-                        id={product.id}
-                        title={product.title}
-                        imageUrl={product.images}
-                        price={product.price}
-                        handleDelete={handleDelete}
-                    />
-                ))
-            }
+            <div className='px-6 overflow-y-scroll flex-1'>
+                {
+                    context.cartProducts.map(product => (
+                        <OrderCard
+                            key={product.id}
+                            id={product.id}
+                            title={product.title}
+                            imageUrl={product.images}
+                            price={product.price}
+                            handleDelete={handleDelete}
+                        />
+                    ))
+                }
             </div>
-            <div className='px-6'>
-                <p className='flex justify-between items-center'>
+            <div className='px-6  mb-6'>
+                <p className='flex justify-between items-center mb-2'>
                     <span className='font-light'>Total:</span>
                     <span className='font-medium text-2xl'>${totalPrice(context.cartProducts)}</span>
                 </p>
+                <button className='bg-black py-3 text-white w-full rounded-lg' onClick={() => handleCheckout()}>Checkout</button>
             </div>
         </aside>
     )
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -16,6 +16,9 @@ export const ShoppingCartProvider = ({ children }) => {
     const [productToShow, setProductToShow] = useState({})
     const [cartProducts, setCartProducts] = useState([])
 
+    // Shopping Cart . Order
+    const [order, setOrder ] = useState([])
+
     return (
         <ShoppingCartContext.Provider value={{
             count,
@@ -29,7 +32,9 @@ export const ShoppingCartProvider = ({ children }) => {
             setCartProducts,
             isCheckoutSideMenuOpen,
             openCheckoutSideMenu,
-            closeCheckoutSideMenu
+            closeCheckoutSideMenu,
+            order,
+            setOrder
         }}>
             {children}
         </ShoppingCartContext.Provider>
