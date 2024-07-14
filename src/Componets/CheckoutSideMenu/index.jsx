aa43507787e856d21a5d7a03ebd744230d36e7df
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-07-14 11:15:02 -0400

    Checkout de productos en el carrito

--- src/Componets/CheckoutSideMenu/index.jsx
+++ src/Componets/CheckoutSideMenu/index.jsx
@@ -4,6 +4,7 @@ import { XMarkIcon } from '@heroicons/react/24/solid'
 import './styles.css'
 import OrderCard from '../OrderCard';
 import { totalPrice } from '../../utils';
+import { Link } from 'react-router-dom';
 
 const CheckoutSideMenu = () => {
     const context = useContext(ShoppingCartContext)
@@ -56,7 +57,9 @@ const CheckoutSideMenu = () => {
                     <span className='font-light'>Total:</span>
                     <span className='font-medium text-2xl'>${totalPrice(context.cartProducts)}</span>
                 </p>
-                <button className='bg-black py-3 text-white w-full rounded-lg' onClick={() => handleCheckout()}>Checkout</button>
+                <Link to='/my-orders/last'>
+                    <button className='bg-black py-3 text-white w-full rounded-lg' onClick={() => handleCheckout()}>Checkout</button>
+                </Link>
             </div>
         </aside>
     )
--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,7 +1,26 @@
+import { useContext } from 'react';
+import { ShoppingCartContext } from '../../Context'
+import OrderCard from '../../Componets/OrderCard';
+
 function MyOrder() {
+    const context = useContext(ShoppingCartContext)
+
     return (
-        <div className='bg-red-200'>
+        <div>
             MyOrder
+            <div className='flex flex-col w-80'>
+                {
+                    context.order?.slice(-1)[0].products.map(product => (
+                        <OrderCard
+                            key={product.id}
+                            id={product.id}
+                            title={product.title}
+                            imageUrl={product.images}
+                            price={product.price}
+                        />
+                    ))
+                }
+            </div>
         </div>
     )
 }
