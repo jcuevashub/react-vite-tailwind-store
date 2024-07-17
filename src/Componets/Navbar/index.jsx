310893e69bdfc38b965a4662cdcbdd775f7d3d8d
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-07-16 20:09:48 -0400

    feat(navbar & sign in): Agregar funcionalidad que muestre ciertos elementos en el Navbar dependiendo del valor de la key de sign-out en LocalStorage y Maquetación de la página de SignIn

--- src/Componets/Navbar/index.jsx
+++ src/Componets/Navbar/index.jsx
@@ -1,15 +1,59 @@
 import { NavLink } from "react-router-dom"
 import { useContext } from 'react'
 import { ShoppingCartContext } from '../../Context'
+import { ShoppingBagIcon } from "@heroicons/react/24/solid"
 
 const Navbar = () => {
     const activeStyle = 'underline underline-offset-4'
     const context = useContext(ShoppingCartContext)
 
+    //Sign Out
+    const signOut = localStorage.getItem('sign-out')
+    const parsedSignOut = JSON.parse(signOut)
+    const isUserSignOut = context.signOut || parsedSignOut
+
     const handleSignOut = () => {
         const stringifiedSignOut = JSON.stringify(true)
         localStorage.setItem('sign-out', stringifiedSignOut)
         context.setSignOut(true)
+    }
+
+    const renderView = () => {
+        if (isUserSignOut) {
+            return (
+                <li>
+                    <NavLink to='/sign-in' className={({ isActive }) => isActive ? activeStyle : undefined}
+                        onClick={() => handleSignOut()}
+                    >
+                        Sign Out
+                    </NavLink>
+                </li>
+            )
+        } else {
+            return (
+                <>
+                    <li className="text-black/60">
+                        jackson@example.com
+                    </li>
+                    <li>
+                        <NavLink to='/my-orders' className={({ isActive }) => isActive ? activeStyle : undefined}>
+                            My Orders
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/my-account' className={({ isActive }) => isActive ? activeStyle : undefined}>
+                            My Account
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/sign-in' className={({ isActive }) => isActive ? activeStyle : undefined}
+                            onClick={() => handleSignOut()}>
+                            Sign Out
+                        </NavLink>
+                    </li>
+                </>
+            )
+        }
     }
 
     return (
@@ -28,12 +72,12 @@ const Navbar = () => {
                 </li>
                 <li>
                     <NavLink to='/mensclothing' onClick={() => context.setSearchByCategory('men\'s clothing')} className={({ isActive }) => isActive ? activeStyle : undefined}>
-                    Men's clothing
+                        Men's clothing
                     </NavLink>
                 </li>
                 <li>
                     <NavLink to='/womensclothing' onClick={() => context.setSearchByCategory('women\'s clothing')} className={({ isActive }) => isActive ? activeStyle : undefined}>
-                    Women's clothing
+                        Women's clothing
                     </NavLink>
                 </li>
                 <li>
@@ -43,35 +87,14 @@ const Navbar = () => {
                 </li>
                 <li>
                     <NavLink to='/jewelery' onClick={() => context.setSearchByCategory('jewelery')} className={({ isActive }) => isActive ? activeStyle : undefined}>
-                    Jewelery
+                        Jewelery
                     </NavLink>
                 </li>
             </ul>
             <ul className="flex items-center gap-3">
-                <li className="text-black/60">
-                    jackson@example.com
-                </li>
-                <li>
-                    <NavLink to='/my-orders' className={({ isActive }) => isActive ? activeStyle : undefined}>
-                        My Orders
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/my-account' className={({ isActive }) => isActive ? activeStyle : undefined}>
-                        My Account
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/sign-in' className={({ isActive }) => isActive ? activeStyle : undefined}
-                        onClick={() => handleSignOut()}
-                    >
-                        Sign Out
-                    </NavLink>
-                </li>
+                {renderView()}
                 <li className="flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-                    </svg>
+                    <ShoppingBagIcon className="h-6 w-6 text-black" />
                     <div>
                         {context.cartProducts.length}
                     </div>
