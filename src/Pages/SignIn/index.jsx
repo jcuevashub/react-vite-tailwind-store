113c3957a58d780a2af8e0944b01163637160fec
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-07-16 20:23:38 -0400

    Reto 4: Agregar estado local para mostrar la vista de crear cuenta o de hacer log in

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,9 +1,10 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { Link } from "react-router-dom"
 import { ShoppingCartContext } from "../../Context"
 
 function SignIn() {
   const context = useContext(ShoppingCartContext)
+  const [view, setView] = useState('user-info')
 
   const account = localStorage.getItem('account')
   const parsedAccount = JSON.parse(account)
@@ -12,9 +13,8 @@ function SignIn() {
   const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0 : true
   const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState
 
-  return (
-    <>
-      <h1 className="font-medium text-xl text-center mb-6 w-80">Welcome</h1>
+  const renderLogIn = () => {
+    return (
       <div className="flex flex-col w-80">
         <p>
           <span className="font-light text-sm">Email: </span>
@@ -36,11 +36,25 @@ function SignIn() {
         </div>
         <button
           className="border border-black disabled:text-black/40 disabled:border-black/40 rounded-lg mt-6 py-3"
+          onClick={() => setView('create-user-info')}
           disabled={hasUserAnAccount}
         >
           Sign up
         </button>
       </div>
+    )
+  }
+
+  const renderCreateUserInfo = () => {
+
+  }
+
+  const renderView = () => view === 'create-user-info' ? renderCreateUserInfo() : renderLogIn()
+
+  return (
+    <>
+      <h1 className="font-medium text-xl text-center mb-6 w-80">Welcome</h1>
+      {renderView()}
     </>
   )
 }
